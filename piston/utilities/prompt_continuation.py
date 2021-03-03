5f6e6c6b591ba69aae816976c140923cdd20c32b
UnrealApex <79121360+UnrealApex@users.noreply.github.com>
2021-03-02 19:11:33 -0800

    Changed Single Line Comments Marked As Multiline Comments

--- piston/utilities/prompt_continuation.py
+++ piston/utilities/prompt_continuation.py
@@ -2,5 +2,5 @@ from piston.utilities.constants import Shell
 
 
 def prompt_continuation(*args) -> str:
-    """Prompt continuation method for prompt_toolkit."""
+    # Prompt continuation method for prompt_toolkit.
     return Shell.promt_continuation
