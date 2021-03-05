e0e41baf47f709aacd6fd1d084a87c335711c987
Shivansh-007 <Shivansh-007@users.noreply.github.com>
2021-03-05 17:29:18 +0530

    Add docstring to split utils function

--- piston/utilities/parse_userinput.py
+++ piston/utilities/parse_userinput.py
@@ -3,6 +3,7 @@ import typing as t
 
 
 def split_unescape(string: str) -> t.List[str]:
+    """Parse string like python string parsing with the help of backslash."""
     parsed_string = shlex.shlex(string, posix=True)
     parsed_string.escapedquotes = "\"'"
     return list(parsed_string)
