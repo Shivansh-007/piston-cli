459ecac808b08256d68fa203c703bebfdd91a8aa
Shivansh-007 <shivansh-007@outlook.com>
2021-04-10 07:38:40 +0530

    Whoops, fix typo.

--- piston/utils/prompt_continuation.py
+++ piston/utils/prompt_continuation.py
@@ -3,4 +3,4 @@ from piston.utils.constants import Shell
 
 def prompt_continuation(*args) -> str:
     """Prompt continuation method for prompt_toolkit."""
-    return Shell.promt_continuation
+    return Shell.prompt_continuation
