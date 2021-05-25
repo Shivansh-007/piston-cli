1375e667b61649a573fdf54318dbad0ea83116b6
Satvik Reddy <reddy.satvik@gmail.com>
2021-05-24 21:01:27 -0700

    fix: order config fixes by category

--- piston/configuration/config_fixer.py
+++ piston/configuration/config_fixer.py
@@ -9,10 +9,9 @@ from piston.configuration.validators.theme_validator import ThemeValidator
 def fix_config(config: dict) -> None:
     """Validates and fixes a configuration dictionary temporarily."""
     config["theme"] = ThemeValidator(config["theme"]).fix_theme()
+    config["box_style"] = BoxStyleValidator(config["box_style"]).fix_box_style()
 
     config["prompt_start"] = PromptStartValidator(config["prompt_start"]).fix_prompt()
-
     config["prompt_continuation"] = PromptContinuationValidator(
         config["prompt_continuation"]
     ).fix_prompt()
-    config["box_style"] = BoxStyleValidator(config["box_style"]).fix_box_style()
